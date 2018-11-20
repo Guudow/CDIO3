23295cfdae2b3d62c3d22897ccdfba041727fffb
guudow <mguudow@aol.com>
2018-11-20 20:58:29 +0100

    mere class

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -5,15 +5,18 @@ public class Player {
     private String name;
     private String spilBrik;
     private String ejendomme;
-    private boolean fængsel;
+    private boolean jail;
     private int position;
+    private boolean balance;
 
-    public Player(String name, String spilBrik, String ejendomme, boolean fængsel, int position) {
+    public Player(String name, String spilBrik, String ejendomme,
+                  boolean jail, int position, boolean balance ) {
         this.name = name;
         this.spilBrik = spilBrik;
         this.ejendomme = ejendomme;
-        this.fængsel = fængsel;
+        this.jail = jail;
         this.position = position;
+        this.balance = balance;
     }
 
     public String getName() {
@@ -40,12 +43,12 @@ public class Player {
         this.ejendomme = ejendomme;
     }
 
-    public boolean isFængsel() {
-        return fængsel;
+    public boolean getJail() {
+        return jail;
     }
 
-    public void setFængsel(boolean fængsel) {
-        this.fængsel = fængsel;
+    public void setjail(boolean jail) {
+        this.jail = jail;
     }
 
     public int getPosition() {
@@ -56,5 +59,34 @@ public class Player {
         this.position = position;
     }
 
+    public boolean getBalance() {
+        return balance;
+    }
 
+    public void setBalance(boolean balance) {
+        this.balance = balance;
+    }
+
+    public boolean checkJail(){
+
+    }
+
+    public int Payent(int rent){
+
+    }
+
+    public int Payproperty(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", spilBrik='" + spilBrik + '\'' +
+                ", ejendomme='" + ejendomme + '\'' +
+                ", Jail=" + jail +
+                ", position=" + position +
+                '}';
+    }
 }
