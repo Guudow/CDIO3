71ece2fd185d32e4fbd33388a05e6664dcb5386b
kbirongo@gmail.com <mgr85wpf>
2018-11-28 16:54:30 +0100

    Sørger for at der kun kan indtastes mellem 2-4 spillere i game klassen

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -4,6 +4,8 @@ import controller.Controller;
 
 import java.util.Scanner;
 
+import java.util.InputMismatchException;
+
 public class Game {
 
     public static void main(String[] args) {
@@ -18,36 +20,45 @@ public class Game {
 
         while (true)
         {
-            int playercount = scanner.nextInt();
-            if (playercount == 2)
-            {
-                numPlayers = 2;
-                Player player1 = new Player(numPlayers, 1);
-                Player player2 = new Player(numPlayers, 2);
-                break;
-            }
-            else if (playercount == 3)
-            {
-                numPlayers = 3;
-                Player player1 = new Player(numPlayers, 1);
-                Player player2 = new Player(numPlayers, 2);
-                Player player3 = new Player(numPlayers, 3);
-                break;
-            }
-            else if (playercount == 4)
-            {
-                numPlayers = 4;
-                Player player1 = new Player(numPlayers, 1);
-                Player player2 = new Player(numPlayers, 2);
-                Player player3 = new Player(numPlayers, 3);
-                Player player4 = new Player(numPlayers, 4);
-                break;
-            }
-            else
-            {
-                System.out.println("Ugyldigt input");
+            //sørger for at der kun kan indtastes mellem 2-4 spillere.
+            try {
+                int playercount = scanner.nextInt();
 
+                if (playercount == 2)
+
+                {
+                    numPlayers = 2;
+                    Player player1 = new Player(numPlayers, 1);
+                    Player player2 = new Player(numPlayers, 2);
+                    break;
+                }
+                else if (playercount == 3)
+                {
+                    numPlayers = 3;
+                    Player player1 = new Player(numPlayers, 1);
+                    Player player2 = new Player(numPlayers, 2);
+                    Player player3 = new Player(numPlayers, 3);
+                    break;
+                }
+                else if (playercount == 4)
+                {
+                    numPlayers = 4;
+                    Player player1 = new Player(numPlayers, 1);
+                    Player player2 = new Player(numPlayers, 2);
+                    Player player3 = new Player(numPlayers, 3);
+                    Player player4 = new Player(numPlayers, 4);
+                    break;
+                }
+                else
+                {
+                    System.out.println("Ugyldigt input antal af spillere skal være mellem 2-4 :)");
+
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Indtast vensligts et HELTAL :)");
+                scanner.next();
             }
+
         }
         Controller controller = new Controller(numPlayers);
 
