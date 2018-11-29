f65de00f5ff25a2085be91c8b1ceab44c4179e23
kbirongo@gmail.com <mgr85wpf>
2018-11-29 12:09:55 +0100

    1. gør det muligt at vælge spiller 2. Gør guien mere overskuelig

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -7,101 +7,85 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Game {
+    
 
     public static void main(String[] args) {
+        Controller controller = new Controller();
+
+
+        /*Scanner scanner = new Scanner(System.in);*/
         int numPlayers = 0;
-        Player[] playerArray;
 
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Vælg antal spillere:");
-
-
-
-        while (true)
-        {
             //user inputs amount of players.
-            try {
-                int playercount = scanner.nextInt();
-
-                if (playercount == 2)
-
-                {
-                    numPlayers = 2;
-                    Player player1 = new Player(numPlayers, 1);
-                    Player player2 = new Player(numPlayers, 2);
-
-                    playerArray = new Player[2];
-                    playerArray[0] = player1;
-                    playerArray[1] = player2;
-                    break;
-                }
-                else if (playercount == 3)
-                {
-                    numPlayers = 3;
-                    Player player1 = new Player(numPlayers, 1);
-                    Player player2 = new Player(numPlayers, 2);
-                    Player player3 = new Player(numPlayers, 3);
-
-                    playerArray = new Player[3];
-                    playerArray[0] = player1;
-                    playerArray[1] = player2;
-                    playerArray[2] = player3;
-                    break;
-                }
-                else if (playercount == 4)
-                {
-                    numPlayers = 4;
-                    Player player1 = new Player(numPlayers, 1);
-                    Player player2 = new Player(numPlayers, 2);
-                    Player player3 = new Player(numPlayers, 3);
-                    Player player4 = new Player(numPlayers, 4);
-
-                    playerArray = new Player[4];
-                    playerArray[0] = player1;
-                    playerArray[1] = player2;
-                    playerArray[2] = player3;
-                    playerArray[3] = player4;
-                    break;
-                }
-                else
-                {
-                    System.out.println("Ugyldigt input antal af spillere skal være mellem 2-4 :)");
-
-                }
-            } catch (InputMismatchException e){
-                System.out.println("Indtast vensligts et HELTAL :)");
-                scanner.next();
-            }
-
-        }
-        Controller controller = new Controller(numPlayers);
-
+        int playercount = controller.getNumPlayers();
+        Player[] playerArray;
         GameBoard gameBoard = new GameBoard();
         GameLogic gameLogic = new GameLogic();
 
+        while(true) {
 
+            if (playercount == 2) {
+                numPlayers = 2;
+                Player player1 = new Player(numPlayers, 1);
+                Player player2 = new Player(numPlayers, 2);
 
+                playerArray = new Player[2];
+                playerArray[0] = player1;
+                playerArray[1] = player2;
 
+            } else if (playercount == 3) {
+                numPlayers = 3;
+                Player player1 = new Player(numPlayers, 1);
+                Player player2 = new Player(numPlayers, 2);
+                Player player3 = new Player(numPlayers, 3);
 
-        /// determans whos turn it is and changes the turn.
-        int turn = 0;
+                playerArray = new Player[3];
+                playerArray[0] = player1;
+                playerArray[1] = player2;
+                playerArray[2] = player3;
 
-        while (turn < numPlayers)
-        {
+            } else if (playercount == 4) {
+                numPlayers = 4;
+                Player player1 = new Player(numPlayers, 1);
+                Player player2 = new Player(numPlayers, 2);
+                Player player3 = new Player(numPlayers, 3);
+                Player player4 = new Player(numPlayers, 4);
 
-            controller.removePlayer(playerArray[turn].getPosition(), turn);
-            gameLogic.movePlayer(playerArray[turn]);
-            controller.setPlayerPosition(playerArray[turn].getPosition(), turn);
+                playerArray = new Player[4];
+                playerArray[0] = player1;
+                playerArray[1] = player2;
+                playerArray[2] = player3;
+                playerArray[3] = player4;
+                break;
 
-            if(turn > numPlayers){
-                turn=0;
             }
-            turn++;
+
+
 
         }
 
 
-    }
-}
+        //try to create a loop that runs the game automatically
+        int turn = 0;
+                   while (turn < playercount)
+                    {
+                            controller.removePlayer(playerArray[turn].getPosition(), turn);
+                            gameLogic.movePlayer(playerArray[turn]);
+                            controller.setPlayerPosition(playerArray[turn].getPosition(), turn);
+
+                            if(turn > playercount){
+                                    turn=0;
+                            }
+                            turn++;
+                    }
+
+        //tester hvordan det virker
+
+
+    }}
+
+
+
+
+
+
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-import gui_codebehind.GUI_FieldFactory;
 import gui_fields.*;
 import gui_main.GUI;
 
@@ -10,39 +9,52 @@ public class Controller {
     GUI gui;
     GUI_Field[] fields;
     GUI_Player[] guiPlayerArray;
+    int numPlayers;
 
-    public Controller(int numPlayers)
+    public Controller()
     {
-        fields = new GUI_Field[24];
-
-        fields[0] = new GUI_Start("Start", "", "Start her", Color.blue, Color.red);
-        fields[1] = new GUI_Street("Burgerbaren", "M1", "Køb burger", "1", Color.red, Color.white);
-        fields[2] = new GUI_Street("Pizzariaet", "M1", "Køb pizza", "1", Color.red, Color.white);
-        fields[3] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
-        fields[4] = new GUI_Street("Slikbutikken", "M1", "Køb slik", "1", Color.lightGray, Color.blue);
-        fields[5] = new GUI_Street("Isbutikken", "M1", "Køb is", "1", Color.lightGray, Color.blue);
-        fields[6] = new GUI_Jail("default", "Fængsel", "Fængsel", "Du er bleven snuppet", Color.black, Color.white);
-        fields[7] = new GUI_Street("Museet", "M2", "Gå på museum", "1", Color.magenta, Color.white);
-        fields[8] = new GUI_Street("Biblioteket", "M2", "Gå på bibilioteket", "1", Color.magenta, Color.white);
-        fields[9] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
-        fields[10] = new GUI_Street("Skateparken", "M2", "Stå på skateboard", "1", Color.orange, Color.blue);
-        fields[11] = new GUI_Street("Poolen", "M2", "Svøm", "1", Color.orange, Color.blue);
-        fields[12] = new GUI_Refuge("default", "Gratis parkering", "", "Vent en tur", Color.white, Color.red);
-        fields[13] = new GUI_Street("Spillehallen", "M3", "Spil", "1", Color.red, Color.white);
-        fields[14] = new GUI_Street("Biografen", "M3", "Se film", "1", Color.red, Color.white);
-        fields[15] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
-        fields[16] = new GUI_Street("Legetøjsbutikken", "M3", "Køb legetøj", "1", Color.orange, Color.blue);
-        fields[17] = new GUI_Street("Dyrehandlen", "M3", "Køb dyreting", "1", Color.orange, Color.blue);
-        fields[18] = new GUI_Jail("default", "Fængsel", "Fængsel", "Du er bleven snuppet", Color.black, Color.white);
-        fields[19] = new GUI_Street("Bowlinghallen", "M4", "Bowling", "1", Color.green, Color.red);
-        fields[20] = new GUI_Street("Zooliskhave", "M4", "Kig på dyr", "1", Color.green, Color.red);
-        fields[21] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
-        fields[22] = new GUI_Street("Vandlandet", "M5", "Svøm", "1", Color.blue, Color.white);
-        fields[23] = new GUI_Street("Strandpromenaden", "M5", "Strand", "1", Color.blue, Color.white);
-
+        //generating the field and initializing gui
+        generateField();
         GUI gui = new GUI(fields);
         this.gui = gui;
 
+        /*Player chooses the amount of things that he or she can do*/
+                int tempplayercount = gui.getUserInteger("indtast antal spillere");
+                while (true) {
+
+                    if (tempplayercount < 2 || tempplayercount > 4) {
+                        tempplayercount = gui.getUserInteger("Der kan kun være 2-4 spillere");
+                    } else {
+                        numPlayers = tempplayercount;
+                        break;
+                    }
+                }
+                amountplay();
+
+                /*game can begin*/
+
+
+
+    }
+
+    public String dicePrompt()
+    {
+        return gui.getUserButtonPressed("1", "Kast Terning");
+    }
+
+    public void setPlayerPosition(int position, int playerNumber)
+    {
+        fields[position].setCar(guiPlayerArray[playerNumber], true);
+    }
+
+    public void removePlayer(int position, int playerNumber)
+    {
+        fields[position].setCar(guiPlayerArray[playerNumber], false);
+    }
+
+    public int getNumPlayers(){return numPlayers;}
+
+    public void amountplay(){
 
         if (numPlayers == 2)
         {
@@ -116,19 +128,39 @@ public class Controller {
 
     }
 
-    public String dicePrompt()
-    {
-        return gui.getUserButtonPressed("1", "Kast Terning");
+    public void generateField(){
+        fields = new GUI_Field[24];
+
+        fields[0] = new GUI_Start("Start", "", "Start her", Color.blue, Color.red);
+        fields[1] = new GUI_Street("Burgerbaren", "M1", "Køb burger", "1", Color.red, Color.white);
+        fields[2] = new GUI_Street("Pizzariaet", "M1", "Køb pizza", "1", Color.red, Color.white);
+        fields[3] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
+        fields[4] = new GUI_Street("Slikbutikken", "M1", "Køb slik", "1", Color.lightGray, Color.blue);
+        fields[5] = new GUI_Street("Isbutikken", "M1", "Køb is", "1", Color.lightGray, Color.blue);
+        fields[6] = new GUI_Jail("default", "Fængsel", "Fængsel", "Du er bleven snuppet", Color.black, Color.white);
+        fields[7] = new GUI_Street("Museet", "M2", "Gå på museum", "1", Color.magenta, Color.white);
+        fields[8] = new GUI_Street("Biblioteket", "M2", "Gå på bibilioteket", "1", Color.magenta, Color.white);
+        fields[9] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
+        fields[10] = new GUI_Street("Skateparken", "M2", "Stå på skateboard", "1", Color.orange, Color.blue);
+        fields[11] = new GUI_Street("Poolen", "M2", "Svøm", "1", Color.orange, Color.blue);
+        fields[12] = new GUI_Refuge("default", "Gratis parkering", "", "Vent en tur", Color.white, Color.red);
+        fields[13] = new GUI_Street("Spillehallen", "M3", "Spil", "1", Color.red, Color.white);
+        fields[14] = new GUI_Street("Biografen", "M3", "Se film", "1", Color.red, Color.white);
+        fields[15] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
+        fields[16] = new GUI_Street("Legetøjsbutikken", "M3", "Køb legetøj", "1", Color.orange, Color.blue);
+        fields[17] = new GUI_Street("Dyrehandlen", "M3", "Køb dyreting", "1", Color.orange, Color.blue);
+        fields[18] = new GUI_Jail("default", "Fængsel", "Fængsel", "Du er bleven snuppet", Color.black, Color.white);
+        fields[19] = new GUI_Street("Bowlinghallen", "M4", "Bowling", "1", Color.green, Color.red);
+        fields[20] = new GUI_Street("Zooliskhave", "M4", "Kig på dyr", "1", Color.green, Color.red);
+        fields[21] = new GUI_Chance("?", "Træk kort", "Træk chancekort", Color.white, Color.yellow);
+        fields[22] = new GUI_Street("Vandlandet", "M5", "Svøm", "1", Color.blue, Color.white);
+        fields[23] = new GUI_Street("Strandpromenaden", "M5", "Strand", "1", Color.blue, Color.white);
+    }
     }
 
-    public void setPlayerPosition(int position, int playerNumber)
-    {
-        fields[position].setCar(guiPlayerArray[playerNumber], true);
-    }
 
-    public void removePlayer(int position, int playerNumber)
-    {
-        fields[position].setCar(guiPlayerArray[playerNumber], false);
-    }
 
-}
+
+
+
+
